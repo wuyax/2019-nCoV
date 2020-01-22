d66f34a6b0925a470ce6172c0b8dbbfe600ed99e
wuyax <wuyax@users.noreply.github.com>
2020-01-22 13:32:27 +0000

    Deploying to gh-pages from master 3746bebc2ccecd7a1cd31b6a038ee864ae7c61ef

---
+++ precache-manifest.7144ffca48ff71e5c0754fda41c03db4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5a8718f41181d2b910a",
+    "url": "/css/app.fcc6ff26.css"
+  },
+  {
+    "revision": "d241df96b3f38062302f",
+    "url": "/css/chunk-vendors.c953cbe6.css"
+  },
+  {
+    "revision": "436231b81c459176e66b272ec328f69d",
+    "url": "/data/info.json"
+  },
+  {
+    "revision": "5b39056731813df5946f699625124291",
+    "url": "/data/ncov.csv"
+  },
+  {
+    "revision": "d145e7ac06e61e37c429374b1fc8602c",
+    "url": "/data/ncov.json"
+  },
+  {
+    "revision": "4d6755f3c0b1b089a3d59caa5bb5cb52",
+    "url": "/data/news.json"
+  },
+  {
+    "revision": "24c5a86d9f7f66a6edd90539adc98777",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0e1e410a52c3fe78fa3",
+    "url": "/js/about.3f5a9305.js"
+  },
+  {
+    "revision": "a5a8718f41181d2b910a",
+    "url": "/js/app.72f42c7c.js"
+  },
+  {
+    "revision": "d241df96b3f38062302f",
+    "url": "/js/chunk-vendors.ad43e3b7.js"
+  },
+  {
+    "revision": "8b92937c0e1aaaec228bc9ab733f583c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
